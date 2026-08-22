dd213891ec62c8176eba46284237c41a9eda36ff
Ayush Kumar <bravoayushkumar@gmail.com>
2026-08-22 16:15:52 +0530

    Add files via upload

---
+++ Arrays_1.java
@@ -0,0 +1,10 @@
+public class Arrays_1 {
+    public static void main(String[] args){
+        int [] x={566,7,3,8,8,5,7,8,8};
+        System.out.println(x);//access
+        x[3]=678;
+        System.out.println(x[3]);
+
+    }
+
+}

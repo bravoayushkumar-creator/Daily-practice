04fd9de2c07e4dda038f8c2149164c89806daf7a
Ayush Kumar <bravoayushkumar@gmail.com>
2026-08-23 12:43:58 +0530

    Add files via upload

---
+++ Aug_2.java
@@ -0,0 +1,12 @@
+public class Aug_2 {
+    public static void main(String[] args){
+        int []arr ={45,67,68,78,67,78,456};
+        int n=arr.length;
+        for(int i=0;i<n;i++){
+            System.out.println(arr[i]+" ");
+
+        }
+
+
+    }
+}

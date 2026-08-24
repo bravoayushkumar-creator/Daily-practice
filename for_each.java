0acd96f241101f4fe59a60b0f0fbf99f332c1df5
Ayush Kumar <bravoayushkumar@gmail.com>
2026-08-24 23:41:36 +0530

    Add files via upload

---
+++ for_each.java
@@ -0,0 +1,13 @@
+public class for_each {
+    public static void main(String[] args){
+        int arr[]={2,45,45,57,6,7,45,};
+        for (int ele:arr){
+            ele=ele*2;
+
+        }
+        for(int ele:arr){
+            System.out.println(ele);
+        }
+
+    }
+}
